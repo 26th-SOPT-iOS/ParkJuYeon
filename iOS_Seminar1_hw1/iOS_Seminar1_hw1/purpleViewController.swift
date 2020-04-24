2447f7d598f2be7f58cda92ac4b287475f98c805
juyeonblue <jyp0211@ajou.ac.kr>
2020-04-24 15:57:49 +0900

    1st seminar assignment

--- iOS_Seminar1_hw1/iOS_Seminar1_hw1/purpleViewController.swift
+++ iOS_Seminar1_hw1/iOS_Seminar1_hw1/purpleViewController.swift
@@ -4,6 +4,7 @@
 //
 //  Created by 박주연 on 18/04/2020.
 //  Copyright © 2020 박주연. All rights reserved.
+file:///Users/parkjuyeon/ParkJuYeon/iOS_Login/iOS_Login/Base.lproj/Main.storyboard: warning: Auto Layout Localization: Views without any layout constraints may clip their content or overlap other views.
 //
 
 import UIKit
